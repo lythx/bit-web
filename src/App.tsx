e36973a2da4f925f891185af7af73e95b41bce14
lythx <lythxxx@gmail.com>
2023-11-17 17:24:40 +0100

    api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const nobelDataFetched = useRef(false)
+  const [nobelData, setNobelData] = useState([])
+  useEffect(() => {
+    if(nobelDataFetched.current) { return }
+    nobelDataFetched.current = true
+    ;(async () => {
+      const res = await fetch('https://api.nobelprize.org/2.1/nobelPrizes')
+      if(!res.ok) {
+        alert(`Fetch error`) // TODO
+        console.log(res)
+        return
+      } 
+      const data = await res.json()
+      setNobelData(data.nobelPrizes)
+    })()
+  }, [nobelData, nobelDataFetched])
+  console.log(nobelData)
   return (
     <div className="App">
     </div>
